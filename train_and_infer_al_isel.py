870a98e5ca9616435743e1add2f473014f74fb58
Jose <jose.dori.nascimento@gmail.com>
2025-01-25 16:59:44 -0300

    new train file

--- train_and_infer_al_isel.py
+++ train_and_infer_al_isel.py
@@ -1,152 +1,196 @@
 import argparse
 import copy
 from datetime import datetime
+import json
 
 from sklearn.model_selection import train_test_split
+from src.gnn.pseudo_labeling import pl_update
 from src.arch.utils import read_setup
 from src.al.al import al_update
-from src.gnn.utils import run_base, generate_graph, validate_best_model
+from src.gnn.utils import generate_graph_hetero, run_base, generate_graph, validate_best_model
 import torch
 import os
 import torch.nn.functional as F
-from src.arch.bnn import BayesianGNN, BayesianHybrid, BayesianMLP
-from src.arch.base import BaseMLP
+from src.arch.bnn import BayesianGNN, BayesianHeteroGNN, BayesianHybrid, BayesianMLP
+from src.arch.base import BaseGNN, BaseMLP
 from tqdm.auto import trange
 from src.utils.constants import DEV_SET, RESULT_FILE, SETUP_FILE, AL_SPLIT_SET, TRAIN_SET, WANDB_NAME
 from src.utils.utils import seed_everything
 import yaml
 import numpy as np
 
-import wandb
-wandb.login()
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+if __name__ == "__main__":
 
-start_time = datetime.now()
-
-
-parser = argparse.ArgumentParser(description="Process command-line arguments")
-parser.add_argument("--exp_id", type=int, required=True, help="Id of the experiment")
-parser.add_argument("--exp_group", type=str, required=True, help="Group of the experiment")
-parser.add_argument("--event", type=str, required=True, help="Name of the event")
-parser.add_argument("--labeled_size", type=int, required=True, help="Size of labeled data")
-parser.add_argument("--set_id", type=int, required=True, help="ID of the data set")
-parser.add_argument("--arch", type=str, default="base_gnn", help="Architecture type")
-parser.add_argument("--arch_setup", type=str, help="Architecture setup")
-parser.add_argument("--device", type=str, default="cpu", help="Device for the experiment")
-parser.add_argument("--run_id", default=0, type=int, required=False)
-parser.add_argument("--reduction", default=None, type=str, required=False, choices=['pca', "autoenc"])
-parser.add_argument("--autoenc", default="ae_base", type=str, required=False)
-parser.add_argument("--pca_red", default=256, type=int, required=False)
-parser.add_argument("--al", default=None, type=str, required=False)
-parser.add_argument("--al_iter", default=3, type=int)
-parser.add_argument("--al_batch", default=10, type=int)
-parser.add_argument("--al_isel", default="random", type=str, required=False)
-parser.add_argument("--aug_unlbl_set", action="store_true", default=False)
-parser.add_argument("--al_random_pseudo_val", action="store_true", default=False)
-parser.add_argument("--retrain", action="store_true", default=False)
-parser.add_argument("--use-cache", action="store_true", default=False)
-
-
-args = parser.parse_args()
-
-kwargs = vars(args)
-
-exp_id = args.exp_id
-exp_group = args.exp_group
-event = args.event
-labeled_size = args.labeled_size
-set_id = args.set_id
-arch = args.arch
-dev_id = args.device
-run_id = args.run_id
-reduction = args.reduction
-al = args.al
-al_iter = args.al_iter
-al_batch = args.al_batch
-al_isel = args.al_isel
-
-if args.aug_unlbl_set:
-    aug_unlbl_set = True
-    kwargs.update({"aug_unlbl_set": True})
-else:
-    aug_unlbl_set = False
-    kwargs.update({"aug_unlbl_set": False})
-
-if args.retrain:
-    retrain = True
-else:
-    retrain = False
-
-if args.al_random_pseudo_val:
-    random_pseudo_val = True
-else:
-    random_pseudo_val = False
-
-if args.arch_setup is not None:
-    arch_setup = args.arch_setup
-else:
-    arch_setup = arch
-
-arch_setup_data = read_setup(arch_setup)
-
-kwargs.update(arch_setup_data)
-
-wandb.init(
-    # Set the project where this run will be logged
-    project="bnn-al", 
-    # We pass a run name (otherwise it’ll be randomly assigned, like sunshine-lollypop-10)
-    name = WANDB_NAME.format(exp_id, event, labeled_size, set_id, run_id),
-    # Track hyperparameters and run metadata
-    config=kwargs
-    )
-
-kwargs.update({"wandb": wandb})
-
-if dev_id is not None:
-    device = torch.device('cuda:{}'.format(dev_id) if torch.cuda.is_available() else 'cpu')
-else:
-    device = 'cpu'
-
-kwargs.update({"device": device})
-
-use_gpu = device != 'cpu'
-
-seed_everything(set_id)
-
-if arch_setup_data["group"] == "mlp":
-    model = BayesianMLP(**kwargs)
-elif arch_setup_data["group"] == "gnn":
-    model = BayesianGNN(**kwargs)
-elif arch_setup_data["group"] == "hybrid":
-    model = BayesianHybrid(**kwargs)
-
-print(kwargs)
-model = model.to(device)
-
-pyg_graph_train, pyg_graph_dev = generate_graph(**kwargs)
-pyg_graph_train = pyg_graph_train.to(device)
-pyg_graph_dev = pyg_graph_dev.to(device)
-
-for i in range(args.al_iter + 1):
+    start_time = datetime.now()
     
-    best_model = run_base(model, pyg_graph_train, **kwargs)
-
-    resf = RESULT_FILE.format(event, exp_group, exp_id, labeled_size, set_id, run_id)
-    os.makedirs(resf[:resf.rfind("/")], exist_ok=True)
-
-    validate_best_model(best_model, pyg_graph_dev, resf, **kwargs)
-
-    # Active Learning
-    if args.al is not None and i < al_iter:
-        print("AL Iteration: {} of {}".format(i+1, al_iter))
-        model = best_model
-
-        pyg_graph_train, pyg_graph_dev = al_update(model, pyg_graph_train, pyg_graph_dev, **kwargs)
-
-        labeled_size += al_batch
+    parser = argparse.ArgumentParser(description="Process command-line arguments")
+    parser.add_argument("--exp_id", type=int, required=True, help="Id of the experiment")
+    parser.add_argument("--exp_group", type=str, required=True, help="Group of the experiment")
+    parser.add_argument("--event", type=str, required=True, help="Name of the event")
+    parser.add_argument("--labeled_size", type=int, required=True, help="Size of labeled data")
+    parser.add_argument("--set_id", type=int, required=True, help="ID of the data set")
+    parser.add_argument("--arch", type=str, default="base_gnn", help="Architecture type")
+    parser.add_argument("--arch_setup", type=str, help="Architecture setup")
+    parser.add_argument("--device", type=str, default="cpu", help="Device for the experiment")
+    parser.add_argument("--run_id", default=0, type=int, required=False)
+    parser.add_argument("--reduction", default=None, type=str, required=False, choices=['pca', "autoenc"])
+    parser.add_argument("--autoenc", default="ae_base", type=str, required=False)
+    parser.add_argument("--pca_red", default=256, type=int, required=False)
+    parser.add_argument("--al", default=None, type=str, required=False)
+    parser.add_argument("--al_iter", default=2, type=int)
+    parser.add_argument("--al_batch", default=16, type=int)
+    parser.add_argument("--al_isel", default="random", type=str, required=False)
+    parser.add_argument("--aug_unlbl_set", action="store", type=str, default=None)
+    parser.add_argument("--al_random_pseudo_val", action="store_true", default=False)
+    parser.add_argument("--dataset_loss", action="store_true", default=False)
+    parser.add_argument("--retrain", action="store_true", default=False)
+    parser.add_argument("--use-cache", action="store_true", default=False)
+    parser.add_argument("--wandb", action="store_true", default=False)
+    parser.add_argument("--epoch_ten", action="store_true", default=False)
+    parser.add_argument("--pl", default=None, type=str, required=False)
+    parser.add_argument("--pl_batch", default=16, type=int)
+    parser.add_argument("--threshold_cluster", default=0, type=int)
+    
+    args = parser.parse_args()
+    
+    kwargs = vars(args)
+    
+    exp_id = args.exp_id
+    exp_group = args.exp_group
+    event = args.event
+    labeled_size = args.labeled_size
+    set_id = args.set_id
+    arch = args.arch
+    dev_id = args.device
+    run_id = args.run_id
+    reduction = args.reduction
+    al = args.al
+    al_iter = args.al_iter
+    al_batch = args.al_batch
+    al_isel = args.al_isel
+    
+    if args.retrain:
+        retrain = True
+    else:
+        retrain = False
+    
+    if args.al_random_pseudo_val:
+        random_pseudo_val = True
+    else:
+        random_pseudo_val = False
+    
+    if args.wandb:
+        wandb_flag = True
+    else:
+        wandb_flag = False
+    
+    if args.arch_setup is not None:
+        arch_setup = args.arch_setup
+    else:
+        arch_setup = arch
+    
+    arch_setup_data = read_setup(arch_setup)
+    
+    hetero = arch_setup_data["hetero"]
+    
+    kwargs.update(arch_setup_data)
+    
+    if args.epoch_ten:
+        kwargs.update({'epochs': 10})
+    
+    kwargs.update({"wand_flag": wandb_flag})
+    if wandb_flag:
+        import wandb
+        wandb.login()
+    
+        wandb.init(
+            # Set the project where this run will be logged
+            project="bnn-al", 
+            # We pass a run name (otherwise it’ll be randomly assigned, like sunshine-lollypop-10)
+            name = WANDB_NAME.format(exp_id, event, labeled_size, set_id, run_id),
+            # Track hyperparameters and run metadata
+            config=kwargs
+            )
+        
+        kwargs.update({"wandb": wandb})
+    
+    if dev_id is not None:
+        device = torch.device('cuda:{}'.format(dev_id) if torch.cuda.is_available() else 'cpu')
+    else:
+        device = 'cpu'
+    
+    kwargs.update({"device": device})
+    
+    use_gpu = device != 'cpu'
+    
+    seed_everything(set_id)
+    
+    if hetero is True:
+        model = BayesianHeteroGNN(**kwargs)
+    elif arch_setup_data["group"] == "mlp":
+        model = BayesianMLP(**kwargs)
+    elif arch_setup_data["group"] == "gnn":
+        model = BayesianGNN(**kwargs)
+    elif arch_setup_data["group"] == "hybrid":
+        model = BayesianHybrid(**kwargs)
+    elif arch_setup_data["group"] == "gnn-base":
+        model = BaseGNN(**kwargs)
+    elif arch_setup_data["group"] == "mlp-base":
+        model = BaseMLP(**kwargs)
+    
+    print(kwargs)
+    model = model.to(device)
+    
+    if hetero is True:
+        pyg_graph_train, pyg_graph_dev = generate_graph_hetero(**kwargs)
+    else:
+        pyg_graph_train, pyg_graph_dev = generate_graph(**kwargs)
+    
+    pyg_graph_train = pyg_graph_train.to(device)
+    pyg_graph_dev = pyg_graph_dev.to(device)
+    
+    for i in range(args.al_iter + 1):
+        
+        best_model = run_base(model, pyg_graph_train, **kwargs)
+    
+        resf = RESULT_FILE.format(event, exp_group, exp_id, labeled_size, set_id, run_id)
+        os.makedirs(resf[:resf.rfind("/")], exist_ok=True)
+    
+        validate_best_model(best_model, pyg_graph_dev, resf, **kwargs)
+    
+        with open(resf, 'r') as file:
+            data_dict = json.load(file)
+    
+        end_time = datetime.now()
+        runtime = end_time - start_time
             
-    if retrain is True:
-        model.reset_parameters()
-
-# Mark the run as finished
-wandb.finish()
+        data_dict['runtime'] = str(runtime.seconds)
+    
+        with open(resf, 'w') as file:
+            json.dump(data_dict, file)
+        
+        start_time = datetime.now()
+    
+        if args.pl is not None and i < al_iter:
+            model = best_model
+            
+            pyg_graph_train, pyg_graph_dev = pl_update(model, pyg_graph_train, pyg_graph_dev, lbl_num=labeled_size, **kwargs)
+    
+        # Active Learning
+        if args.al is not None and i < al_iter:
+            print("AL Iteration: {} of {}".format(i+1, al_iter))
+            model = best_model
+    
+            pyg_graph_train, pyg_graph_dev = al_update(model, pyg_graph_train, pyg_graph_dev, lbl_num=labeled_size, **kwargs)
+    
+            labeled_size += al_batch
+                
+        if retrain is True:
+            model.reset_parameters()
+    
+    
+    if wandb_flag:
+        # Mark the run as finished
+        wandb.finish()
